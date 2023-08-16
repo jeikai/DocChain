a243a7a5cebfbf076e7289d8813a35df973ef637
unknown <dvc.290603@gmail.com>
2023-08-16 21:23:44 +0700

    updated_fe

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import Upload from "../pages/Upload/Upload";
-
+import Dashboard from "../pages/Dashboard/Dashboard";
 const routes = [
-    {path: '/upload', component: Upload, title: 'Upload'}
+    {path: '/upload', component: Dashboard, title: 'Upload'}
 ]
 
 export {routes}
